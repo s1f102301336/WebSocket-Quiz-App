d9410688c4bf231cc84de73a8de7016e54e3b1a7
1f10230237 <s1f102302371@iniad.org>
2025-01-09 17:41:50 +0900

    login中とわかるようににメッセージを追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('create_account/', views.create_accounts, name='create_account'),
+    path('create_account/', views.create_accounts, name='create_account'),  # 関数名をそのまま
+    path('home/', views.home, name='home'),  # ホーム画面
 ]
+
